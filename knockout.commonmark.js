a683c94a2935eda2a95e597dc4d1b046a8a6893d
Mike Lowen <mike@mlowen.com>
2014-12-06 10:42:49 +1300

    Working on the bindings
    Tidying up the code and making sure they work with more than just
    observables and computed.

--- knockout.commonmark.js
+++ knockout.commonmark.js
@@ -3,15 +3,11 @@ define([ 'knockout', 'commonmark' ], function(ko, commonmark) {
 	var writer = commonmark.HtmlRenderer();
 
 	var convert = function (element, valueAccessor) {
-		var html = '';
-		var value = valueAccessor()();
+		var accessor = valueAccessor();
+		var actual = typeof accessor === 'function' ? accessor() : accessor;
 
-		if (value) {
-			html = writer.render(reader.parse(value));
-
-			element.innerHTML = html;
-		};
-	}
+		element.innerHTML = actual ? writer.render(reader.parse(actual)) : '';
+	};
 
 	ko.bindingHandlers.commonmark = { init: convert, update: convert };
 });
