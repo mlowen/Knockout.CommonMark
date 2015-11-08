b20ba2d0aed7d7b0928f938533ff0adf0e786b21
Mike Lowen <mlowen@lic.co.nz>
2015-11-08 14:57:05 +1300

    Added 'dist' gulp task
    Added a gulp task for creating a distribution archive.

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,28 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
 var uglify = require('gulp-uglify');
+var zip = require('gulp-zip');
 
-gulp.task('install:bower', function() { bower(); });
+var meta = require('./package.json');
 
-gulp.task('minify:scripts', function() {
-	gulp.src('./src/*.js')
+gulp.task('bower', function() { return bower(); });
+
+gulp.task('minify', function() {
+	return gulp.src('./src/*.js')
 		.pipe(uglify())
 		.pipe(gulp.dest('./dist'));
 });
 
+gulp.task('dist', [ 'minify' ], function() {
+	return gulp.src([ './dist/knockout.commonmark.js', './README.md', './LICENSE' ])
+		.pipe(zip(meta.name + '-' + meta.version + '.zip'))
+		.pipe(gulp.dest('./'));	
+});
+
 // Tasks
 
-gulp.task('default', [ 'install:bower', 'minify:scripts' ]);
+gulp.task('default', [ 'bower', 'minify' ]);
 
 gulp.task('watch', function() {
-	gulp.watch('./src/*.js', [ 'minify:scripts' ]);
+	gulp.watch('./src/*.js', [ 'minify' ]);
 });
